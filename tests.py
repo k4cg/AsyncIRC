9166116cf43de80aea0016b78b70d44e4789810b
Poschi <development@poschi.net>
2019-01-30 21:07:18 +0100

    Updated username and nickname regex, extended tests

--- tests.py
+++ tests.py
@@ -5,10 +5,11 @@ class IRCBotTest(unittest.TestCase):
     def test_nick_recognition(self):
         nicks = ["abc", "b-s[", "ChanServ", "C_C", "CC[m]", "C-K", "LXI123", "J_", "k4cg2", "n|m", "n[m]"]
         for nick in nicks:
-            input = ":%(username)s!Name@host COMMAND <ARGS>" % {"username": nick} #:User!Name@host COMMAND <ARGS>nick!username@host" 
+            input = ":%(username)s!%(nickname)s@host COMMAND <ARGS>" % {"username": nick, "nickname": nick} #:User!Name@host COMMAND <ARGS>nick!username@host" 
             userhost = user_re.search(input)
-            newNick, user, host = userhost.groups()
-            self.assertEqual(nick, newNick, 'incorrect username recognition: ' + nick + " was " + newNick)
+            newNick, newUser, host = userhost.groups()
+            self.assertEqual(nick, newNick, 'incorrect nickname recognition: ' + nick + " was " + newNick)
+            self.assertEqual(nick, newUser, 'incorrect username recognition: ' + nick + " was " + newUser)
 
 if __name__ == '__main__':
     unittest.main()
